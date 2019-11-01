996e81ff59a37363b335fffccd82ee159df65cc8
omoshthegeek <omoshthegeek@gmail.com>
2019-11-01 15:02:59 +0300

    added jquery and added portfolio overlay styling

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".toggle").click(function () {
+      $(".p-show").toggle();
+    });
+  })
+  
+  $(document).ready(function(){
+    $(".portfolio").hover(function () {
+      $("")
+    }
+  }
